4b76908f5ba7dac35f097fe27f10eeb38d3afb5b
HuTaoBypass <109981350+HuTaoBypass@users.noreply.github.com>
2023-09-17 15:08:57 -0500

    Fixed Register

--- server/process.php
+++ server/process.php
@@ -3,53 +3,81 @@
 require_once 'database.php';
 session_start();
 
-
-switch ($_POST['type']){
-    case 'login':
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        login($username, $password);
-        break;
-    case 'register':
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        $password2 = $_POST['password2'];
-        register($username, $password, $password2);
-        break;        
-    default:
-        echo 'error';
-        break;
-
+if (isset($_GET['type'])) {
+    switch ($_GET['type']) {
+        case 'login':
+            if (isset($_POST['username']) && isset($_POST['password'])) {
+                $username = $_POST['username'];
+                $password = $_POST['password'];
+                login($username, $password);
+            } else {
+                echo 'error';
+            }
+            break;
+        case 'register':
+            if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['password2'])) {
+                $username = $_POST['username'];
+                $password = $_POST['password'];
+                $password2 = $_POST['password2'];
+                register($username, $password, $password2);
+            } else {
+                echo 'error';
+            }
+            break;
+        default:
+            echo 'error';
+            break;
+    }
+} else {
+    echo 'error';
 }
 
 
-function login($user, $password){
+function login($user, $password) {
     $db = new Database();
 
     $db->query("SELECT * FROM users WHERE username = :username");
-
     $db->bind(':username', $user);
     $db->execute();
     $results = $db->resultset();
 
-    if($results->rowCount() > 0){
-        $row = $results->fetch(PDO::FETCH_OBJ);
-        if(password_verify($password, $row['password'])){
+    if ($results ) {
+        $row = $results->fetch(PDO::FETCH_ASSOC);
+        if (password_verify($password, $row['password'])) {
             $_SESSION['username'] = $row['username'];
             $_SESSION['id'] = $row['id'];
             header('Location: index.php');
-        }
-        else{
+            exit; // Add exit to stop further execution
+        } else {
             echo 'error';
         }
+    } else {
+        echo 'error';
     }
 }
-function register($user, $password, $password2){
 
+function register($user, $password, $password2) {
+    $db = new Database();
 
+    $db->query("SELECT * FROM users WHERE username = :username");
+    $db->bind(':username', $user);
+    $db->execute();
+    $results = $db->resultset();
+    echo $results;
+    if ($results['username'] == $user) {
+        echo 'Username already exists.';
+    } elseif ($password == $password2) {
+        $hashedPassword = md5($password);
 
-
+        $db->query("INSERT INTO users (username, password) VALUES (:username, :password)");
+        $db->bind(':username', $user);
+        $db->bind(':password', $hashedPassword);
+        $db->execute();
+        $_SESSION['username'] = $user;
+        header('Location: index.php');
+        exit;
+    } else {
+        echo 'Passwords do not match.';
+    }
 }
-
-
 ?>
