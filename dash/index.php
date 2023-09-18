962f8982987c9347a3219f9641040948ae561581
HuTaoBypass <109981350+HuTaoBypass@users.noreply.github.com>
2023-09-18 18:56:38 -0500

    Update index.php

--- dash/index.php
+++ dash/index.php
@@ -8,23 +8,6 @@ if (!isset($_SESSION['username'])){
 ?>
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.15/dist/tailwind.min.css" rel="stylesheet">
-</head>
-<body class="bg-gray-100">
-    <div class="min-h-screen flex items-center justify-center">
-        <div class="max-w-md bg-white p-8 rounded-lg shadow-md w-full">
-            <h2 class="text-2xl text-indigo-500 font-semibold mb-6">Welcome, <?php echo $_SESSION['username'] ?></h2>
-            
-        </div>
-    </div>
-</body>
-</html>
 
 <!DOCTYPE html>
 <html lang="en">
